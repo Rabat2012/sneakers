2989fdb9ae1de9a398dc01a67ca2717dd69abe58
Rick <rabat.imankulov2001@gmail.com>
2022-07-13 18:22:04 +0600

    header, routing changes

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Brands from "./components/Brands/Brands";
+import HomePage from "./components/HomePage/HomePage";
+import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
 
 const Routing = () => {
-  return <Routes>ewew</Routes>;
+  return (
+    <Routes>
+      <Route path="/brands" element={<Brands />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
 };
 
 export default Routing;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,34 @@
 import React from "react";
 import logo from "../../assets/logo.svg";
 import "./Header.css";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <div>
       <header className="header">
         <nav className="header__nav">
-          <h3 className="header__nav_link">Brands</h3>
-          <img className="header__nav_img" src={logo} alt="logo" />
-          <h3 className="header__nav_link">Account</h3>
+          <h3 onClick={() => navigate("/brands")} className="header__nav_link">
+            Brands
+          </h3>
+          <img
+            onClick={() => navigate("/")}
+            className="header__nav_img"
+            src={logo}
+            alt="logo"
+          />
+          <div className="account">
+            <h3 onClick={() => navigate("/login")} className="header__nav_link">
+              Login
+            </h3>
+
+            <h3
+              onClick={() => navigate("/register")}
+              className="header__nav_link">
+              Register
+            </h3>
+          </div>
         </nav>
       </header>
     </div>
