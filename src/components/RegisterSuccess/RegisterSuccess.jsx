05052781858fc1c375d8dd7f3714efd4fbdd6cc7
Rick <rabat.imankulov2001@gmail.com>
2022-07-19 14:41:00 +0600

    register success

--- src/components/RegisterSuccess/RegisterSuccess.jsx
+++ src/components/RegisterSuccess/RegisterSuccess.jsx
@@ -1,18 +1,14 @@
 import { Box, Container, Typography } from "@mui/material";
 import React, { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { authContext } from "../../contexts/authContext";
 
 const RegisterSuccess = () => {
-  useEffect(() => {
-    setSuccess(true);
-  }, []);
-
   return (
     <Container>
       <Box>
         <Typography>
-          <Link to="/login">войти</Link> в
+          Вы успешно зарегистрировались, можете
+          <Link to="/login"> войти</Link> в свой аккаунт
         </Typography>
       </Box>
     </Container>
