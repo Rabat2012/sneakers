ef869aea1652860972ad739a041c3a860f3afc8c
Rick <rabat.imankulov2001@gmail.com>
2022-07-22 04:00:37 +0600

    sort by category

--- src/components/Brand/Nike.jsx
+++ src/components/Brand/Nike.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { productsContext } from "../../contexts/productsContext";
+import ProductsCard from "../ProductsCard/ProductsCard";
+import "./Brands.css";
 
 const Nike = () => {
-  return <div>Nike</div>;
+  const { getProducts, products } = useContext(productsContext);
+  useEffect(() => {
+    getProducts();
+  }, []);
+  console.log(products);
+  return (
+    <div className="disp">
+      {products.map(item =>
+        item.brand == "Nike" ? <ProductsCard key={item.id} item={item} /> : null
+      )}
+    </div>
+  );
 };
 
 export default Nike;
